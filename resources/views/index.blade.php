5399ee4a710ed516a5785fc0fc7d5f21e750731f
Aknurdaulet Sarkytbayev <63612206+aknrdlt@users.noreply.github.com>
2021-12-18 20:27:45 +0600

    xx

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Комплектстройдеталь</title>
+    <title>Комплектстройдетальxxx</title>
     <link rel="stylesheet" href="style.css">
     <link href="Fonts/stylesheet.css" rel="stylesheet">
     <link rel="stylesheet" href="Fonts/PT Root UI_Bold.css">
